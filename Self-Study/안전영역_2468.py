e8df34e4075374f0b493bc40ca3295824217d8b9
johyunsub <gustjq2167@naver.com>
2021-07-06 20:10:12 +0900

    [BOJ 안전영역] 21.07.06

--- Self-Study/안전영역_2468.py
+++ Self-Study/안전영역_2468.py
@@ -1,7 +1,8 @@
 import collections
+import copy
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
-max_rain = max([max(i) for i in graph])
+max_rain = max([max(i) for i in graph]) #Graph에서 가장 큰 값
 
 def bfs(r, c, vc, lvl, graph):
     global v
@@ -13,25 +14,25 @@ def bfs(r, c, vc, lvl, graph):
         for nr, nc in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]:
             if not (0 <= nr < n and 0 <= nc < n):
                 continue
-            if vc[nr][nc]:
-                continue
-            if graph[nr][nc] == lvl:
+            if graph[nr][nc] == lvl:    #잠긴 수면이면 방문체크
                 v[nr][nc] = True
+                continue
+            if vc[nr][nc]:
                 continue
             q.append([nr, nc])
             vc[nr][nc] = True
 
-res = 0
+res = 1
 v = [[False]*n for _ in range(n)]
-
 for r in range(1, max_rain):
     cnt = 0
-    vc = v.copy()
+    vc = copy.deepcopy(v)   #임시 방문체크 행렬
     for i in range(n):
         for j in range(n):
             if graph[i][j] == r:
                 v[i][j] = True
-            if graph[i][j] > r and not vc[i][j]:
+                continue
+            if not vc[i][j]:
                 bfs(i, j, vc, r, graph)
                 cnt += 1
     res = max(res, cnt)
