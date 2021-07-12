6515179795b8ff5c48cf3983e5317d6a646318c6
johyunsub <gustjq2167@naver.com>
2021-07-13 01:08:43 +0900

    [BOJ 2579] 21.07.13

--- Self-Study/계단오르기_2579.py
+++ Self-Study/계단오르기_2579.py
@@ -3,15 +3,11 @@ stair = [0]*n
 for i in range(n):
     stair[i] = int(input())
 
-dp = [0]*301
-dp[n-1] = stair[n-1]
-dp[n - 2] = stair[n - 1] + stair[n - 2]
-if n == 3:
-    dp[n - 3] = max(stair[n-1] + stair[n-2], stair[n - 1] + stair[n - 3])
-if n > 3:
-    for i in range(n-4, -1, -1):
-        max_ = max(stair[i] + stair[i+1] + dp[i+3], stair[i] + dp[i+2])
-        if dp[i+1] <= max_:
-            dp[i] = max_
+dp = [0]*n
+dp[0] = stair[0]
+if n > 1:
+    dp[1] = stair[0] + stair[1]
+    for i in range(2, n):
+        dp[i] = max(stair[i] + stair[i-1] + dp[i-3], stair[i] + dp[i-2])
 
-print(dp[0])
+print(dp[-1])
