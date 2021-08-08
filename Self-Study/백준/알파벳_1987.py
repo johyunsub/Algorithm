533f3fff8d7f275b8281a02db7e3be0a928a3d4e
johyunsub <gustjq2167@naver.com>
2021-08-08 18:14:26 +0900

    [Refactor] BOJ 1987

--- Self-Study/백준/알파벳_1987.py
+++ Self-Study/백준/알파벳_1987.py
@@ -1,21 +1,27 @@
 R, C = map(int, input().split())
 grp = [list(map(lambda x: ord(x) - 65, input())) for _ in range(R)]
-visit = set()
+V = [False] * 26
 max_ = 0
 
 
-def dfs(r, c, dep):
+def dfs(r, c, dep, v):
     global max_
-    visit.add(grp[r][c])
-    for nr, nc in [(r + 1, c), (r - 1, c), (r, c - 1), (r, c + 1)]:
-        if not (0 <= nr < R and 0 <= nc < C):
-            continue
-        if grp[nr][nc] in visit:
-            continue
-        dfs(nr, nc, dep + 1)
-        visit.remove(grp[nr][nc])
+    v[grp[r][c]] = True
     max_ = max(max_, dep)
 
+    if r - 1 >= 0 and not v[grp[r - 1][c]]:
+        dfs(r - 1, c, dep + 1, v)
+        v[grp[r - 1][c]] = False
+    if r + 1 < R and not v[grp[r + 1][c]]:
+        dfs(r + 1, c, dep + 1, v)
+        v[grp[r + 1][c]] = False
+    if c - 1 >= 0 and not v[grp[r][c - 1]]:
+        dfs(r, c - 1, dep + 1, v)
+        v[grp[r][c - 1]] = False
+    if c + 1 < C and not v[grp[r][c + 1]]:
+        dfs(r, c + 1, dep + 1, v)
+        v[grp[r][c + 1]] = False
 
-dfs(0, 0, 1)
+
+dfs(0, 0, 1, V)
 print(max_)
